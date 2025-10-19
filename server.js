c341ca0354dfb958b2002a3f7d39480ab4f95535
Uday032003 <udayatanakala@gmail.com>
2025-10-19 15:08:22 +0530

    Change nodemailer auth
    Updated email credentials for nodemailer transport.

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ initializeDBAndServer();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: "kgpiitianganesha@gmail.com",
+    pass: "zjyvkybqcjikkjei",
   },
 });
 
