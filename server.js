c26d7eefa62232a0e96cb37169ff2fc92b037429
Uday032003 <udayatanakala@gmail.com>
2025-11-01 12:46:37 +0530

    Change email service to smtp-relay.brevo.com

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ const initializeDBAndServer = async () => {
 initializeDBAndServer();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: 'smtp-relay.brevo.com',
+  port:587,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
